29e7efe98b14f73376bf6e614e07dd649dc19c39
jothank <jothank@hotmail.com>
2022-03-30 23:38:25 -0300

    up

--- atividade_python/profissoes.py
+++ atividade_python/profissoes.py
@@ -17,12 +17,12 @@ class Profissoes():
 
     def salarioMensal(self):
         ##self.salario = float(input('Digite Salario R$'))
-        print(f'Salário é {self.salario}')
+        print(f'Salário é R${self.salario}')
 
     def horas_faltas(self):
         self.valor_horas_falta = (self.salario/220)
 
-    def hroas_extras(self):
+    def horas_extras(self):
         self.valor_horas_extras = (self.salario/220)*0.5+(self.salario/220)
         print(f'Ganhará R${self.valor_horas_extras:.2f} por horas em horas extras conforme salario mensal R${self.salario:.2f}')
 
@@ -30,19 +30,19 @@ class Profissoes():
         ##self.cargo = input('Selecione cargo 1 - Anestesista, 2 - Cirurgiao, 3 - Enfermeiro, 4 - Médico ou 5 - Recepcionista: ')
         if self.cargo == '1':
             self.bonificacao = self.salario*0.1
-            print(f'Bonificação é 10% receberá como Suporte é R${self.bonificacao:.2f} conforme salario mensal de R${self.salario:.2f}')
+            print(f'Bonificação é 10% receberá como Anestesista é R${self.bonificacao:.2f} conforme salario mensal de R${self.salario:.2f}')
         elif self.cargo == '2':
             self.bonificacao = self.salario*0.15
-            print(f'Bonificação é 15% receberá como Professor é R${self.bonificacao:.2f} conforme salario mensal de R${self.salario:.2f}')
+            print(f'Bonificação é 15% receberá como Cirurgiao é R${self.bonificacao:.2f} conforme salario mensal de R${self.salario:.2f}')
         elif self.cargo == '3':
             self.bonificacao = self.salario*0.2
-            print(f'Bonificação é 20% receberá como Programador é R${self.bonificacao:.2f} conforme salario mensal de R${self.salario:.2f}')
+            print(f'Bonificação é 20% receberá como Enfermeiro é R${self.bonificacao:.2f} conforme salario mensal de R${self.salario:.2f}')
         elif self.cargo == '4':
             self.bonificacao = self.salario*0.3
-            print(f'Bonificação é 30% receberá como Gerente é R${self.bonificacao:.2f} conforme salario mensal de R${self.salario:.2f}')
+            print(f'Bonificação é 30% receberá como Médico é R${self.bonificacao:.2f} conforme salario mensal de R${self.salario:.2f}')
         elif self.cargo == '5':
             self.bonificacao = self.salario*0.15
-            print(f'Bonificação é 15% receberá como Analista é R${self.bonificacao:.2f} conforme salario mensal de R${self.salario:.2f}')
+            print(f'Bonificação é 15% receberá como Recepcionista é R${self.bonificacao:.2f} conforme salario mensal de R${self.salario:.2f}')
         else:
             print('Erro! digite um numero de cargo correto')
 
@@ -74,3 +74,27 @@ class Profissoes():
             print(f'Quantidade de horas trabalhada quando possuir 25 anos será {self.calcula_horas_anos_25:.2f} horas, considerando {self.hora_por_dia:.2f} horas dia.')
         else:
             print('Erro! digite o sexo correto')
+
+    def exibir_cargo(self):
+        if self.cargo == '1':
+            print(f'Cargo do funcionario é Anestesista')
+        elif self.cargo == '2':
+            print(f'Cargo do funcionario é Cirurgiao')
+        elif self.cargo == '3':
+            print(f'Cargo do funcionario é Enfermeiro')
+        elif self.cargo == '4':
+            print(f'Cargo do funcionario é Médico')
+        elif self.cargo == '5':
+            print(f'Cargo do funcionario é Recepcionista')
+        else:
+            print('Erro! digite um numero de cargo correto')
+
+    def horas_almoço(self):
+        if self.hora_por_dia == 8:
+            print(f'Funcionario terá direito a 01:00 hora de almoço')
+        elif self.hora_por_dia == 6:
+            print(f'Funcionario terá direito a 00:15 minutos de almoço')
+        elif self.hora_por_dia == 4:
+            print(f'Funcionario não terá direito a almoço')
+        else:
+            print('Erro! digite a quantidade de horas dia correta')
