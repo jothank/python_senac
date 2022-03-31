c80af9a330e76ff0dc59b7ec7e7a567c8d4f838e
jothank <jothank@hotmail.com>
2022-03-30 23:40:22 -0300

    up

--- atividade_python/__anestesista.py
+++ atividade_python/__anestesista.py
@@ -1,5 +1,6 @@
 import profissoes
 
+
 class Anestesista(profissoes.Profissoes):
     def __init__(self, nome_empregado, salario, hora_por_dia, cargo, sexo):
         super().__init__(nome_empregado, salario, hora_por_dia, cargo, sexo)
--- atividade_python/__enfermeiro.py
+++ atividade_python/__enfermeiro.py
@@ -1,5 +1,6 @@
 import profissoes
 
+
 class Enfermeiro(profissoes.Profissoes):
     def __init__(self, nome_empregado, salario, hora_por_dia, cargo, sexo):
         super().__init__(nome_empregado, salario, hora_por_dia, cargo, sexo)
